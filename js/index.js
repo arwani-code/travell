b4b5d08d859a9de4751b8111f83e3dba08dd5610
Ahmad Arwani <64183538+arwani-code@users.noreply.github.com>
2020-12-19 19:21:20 +0700

    catatan perubahan

--- js/index.js
+++ js/index.js
@@ -3,17 +3,21 @@ const closeModal = document.querySelector('.closeBtn')
 const btn = document.querySelector('.btn')
 const firstName = document.querySelector('.firstName')
 const lastName = document.querySelector('.lastName')
-const firstValue = document.querySelector('.first-value')
-const lastValue = document.querySelector('.last-value')
+const phone = document.querySelector('.phone')
+const email = document.querySelector('.email')
 
 btn.addEventListener('click', () => {
-  const firstNameValue = firstName.value
-  const lastNameValue = lastName.value
-  firstValue.innerHTML = firstNameValue
-  lastValue.innerHTML = lastNameValue
+  getDataValue()
   modal.style.display = 'block'
 })
 
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none'
 })
+
+const getDataValue = () => {
+  document.querySelector('.first-value').innerHTML = firstName.value
+  document.querySelector('.last-value').innerHTML = lastName.value
+  document.querySelector('.phone-value').innerHTML = phone.value
+  document.querySelector('.email-value').innerHTML = email.value
+}
