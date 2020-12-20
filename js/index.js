cff9b5927735e1ab4d1ea505a7b92396e761048a
Ahmad Arwani <64183538+arwani-code@users.noreply.github.com>
2020-12-20 19:42:36 +0700

    perubahan contact

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const modal = document.getElementById('simpleModal')
 const closeModal = document.querySelector('.closeBtn')
+const closePopup = document.querySelector('.close-popup')
 const btn = document.querySelector('.btn')
 const firstName = document.querySelector('.firstName')
 const lastName = document.querySelector('.lastName')
@@ -7,12 +8,24 @@ const phone = document.querySelector('.phone')
 const email = document.querySelector('.email')
 
 btn.addEventListener('click', () => {
-  getDataValue()
-  modal.style.display = 'block'
+  if (
+    firstName.value === '' ||
+    lastName.value === '' ||
+    phone.value === '' ||
+    email.value === ''
+  ) {
+    document.querySelector('#popup').style.display = 'block'
+  } else {
+    getDataValue()
+  }
 })
 
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none'
+})
+
+closePopup.addEventListener('click', () => {
+  document.querySelector('#popup').style.display = 'none'
 })
 
 const getDataValue = () => {
@@ -20,4 +33,5 @@ const getDataValue = () => {
   document.querySelector('.last-value').innerHTML = lastName.value
   document.querySelector('.phone-value').innerHTML = phone.value
   document.querySelector('.email-value').innerHTML = email.value
+  modal.style.display = 'block'
 }
